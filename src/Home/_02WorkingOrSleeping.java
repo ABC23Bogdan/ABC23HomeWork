53da860eac6e1c575e9e3a1919c93610819f7636
ABC23Bogdan <spaqgonzo@gmail.com>
2017-04-03 23:22:33 +0400

    Some bad commit with Idea files

---
+++ src/Home/_02WorkingOrSleeping.java
@@ -0,0 +1,19 @@
+package Home;
+import java.util.Scanner;
+
+/**
+ * Created by Asus on 03.04.2017.
+ */
+public class _02WorkingOrSleeping {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter time: ");
+        int t =sc.nextInt();
+       if(t >= 0 && t <= 24 && t >= 9 && t <=18 ){
+           System.out.println("Im at work.");
+       }
+       if(t >= 0 && t <= 24 && t <= 9 && t >= 18 ) {
+           System.out.println("Im sleeping.");
+       }
+    }
+}
