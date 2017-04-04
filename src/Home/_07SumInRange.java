4b0c4a851fec7fe8dfcbc45879998c3ecc0b25ac
ABC23Bogdan <spaqgonzo@gmail.com>
2017-04-04 21:27:15 +0400

    Some bad commit with Idea files

--- src/Home/_07SumInRange.java
+++ src/Home/_07SumInRange.java
@@ -1,23 +1,23 @@
 package Home;
-
 import java.util.Scanner;
-
 
 /**
  * Created by Asus on 04.04.2017.
  */
-public class _06TwoNumbers {
+public class _07SumInRange {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int sum = n1 + n2;
-        int dif = n1 - n2;
 
-        if (n1 > n2) {
-            System.out.println(dif);
-        } else if (n2 > n1) {
-            System.out.println(sum);
+        if (sum >= 11 && sum <= 19 ){
+            System.out.println("Sum in range of 11 to 19 : "+sum);
+        }
+        else {
+            System.out.println("Sum isn't in range. ");
         }
     }
 }
+
